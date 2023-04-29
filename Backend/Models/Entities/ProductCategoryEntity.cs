0576c62083272bdae804ab9ad47e53a32d26cb27
ikueohira <ikue.ohira@gmail.com>
2023-04-29 14:16:54 +0200

    Put it back

--- Backend/Models/Entities/ProductCategoryEntity.cs
+++ Backend/Models/Entities/ProductCategoryEntity.cs
@@ -2,16 +2,7 @@
 {
     public class ProductCategoryEntity
     {
-        //public int Id { get; set; }
-
-        // Ikue added line 7-12
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public string Name { get; set; } = null!;
-        public string? Description { get; set; }
-        public decimal Price { get; set; }
-        public string? Specification { get; set; }
-
-
+        public int Id { get; set; }
         public string CategoryName { get; set; } = null!;
         public ICollection<ProductEntity> Products { get; set; } = new List<ProductEntity>();
 
