9516049b84b3558ebfeb43800d2e53d1c3f45c5d
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-14 21:23:37 +0200

    Added OrderController, OrderEntity and UserProfileModel

---
+++ Backend/Models/Entities/OrderEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models.Entities
+{
+    public class OrderEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public decimal? Price { get; set; }
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+        public UserProfileModel Profile { get; set; } = null!;
+        public List<ProductEntity> Products { get; set; } = new List<ProductEntity>();
+        public string? OrderStatus { get; set; }
+        public string PaymentMethod { get; set; } = null!;
+    }
+}
---
+++ Backend/Models/UserProfileModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend.Models
+{
+    public class UserProfileModel
+    {
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
+        public string StreetName { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+        public string City { get; set; } = null!;
+    }
+}
