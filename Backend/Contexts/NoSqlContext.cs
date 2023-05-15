be550d8658271ec8832525940509b24e99d8aefb
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-15 09:09:45 +0200

    updating nosqlcontext

--- Backend/Contexts/NoSqlContext.cs
+++ Backend/Contexts/NoSqlContext.cs
@@ -10,16 +10,16 @@ namespace Backend.Contexts
         }
 
         public DbSet<ProductEntity> ProductsCatalog { get; set; }
-        //public DbSet<OrderEntity> Orders { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ProductEntity>()
                 .ToContainer("productCatalog")
                 .HasPartitionKey(x => x.Id);
 
-            //modelBuilder.Entity<ProductEntity>()
-            //    .ToContainer("orders")
-            //    .HasPartitionKey(x => x.Id);
+            modelBuilder.Entity<ProductEntity>()
+                .ToContainer("orders")
+                .HasPartitionKey(x => x.Id);
         }
     }
 }
