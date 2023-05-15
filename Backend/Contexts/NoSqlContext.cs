6aeaccd2df4ed75c0aa9fb197ef215af2b631fb6
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-15 08:59:47 +0200

    Added orders in NoSQLContext

--- Backend/Contexts/NoSqlContext.cs
+++ Backend/Contexts/NoSqlContext.cs
@@ -10,10 +10,15 @@ namespace Backend.Contexts
         }
 
         public DbSet<ProductEntity> ProductsCatalog { get; set; }
+        public DbSet<OrderEntity> Orders { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ProductEntity>()
                 .ToContainer("productCatalog")
+                .HasPartitionKey(x => x.Id);
+
+            modelBuilder.Entity<ProductEntity>()
+                .ToContainer("orders")
                 .HasPartitionKey(x => x.Id);
         }
     }
