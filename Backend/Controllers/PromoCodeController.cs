5a130492478503202d68e727c102465ce9631e68
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-17 15:28:27 +0200

    Added GettAll on PromoCodeController

--- Backend/Controllers/PromoCodeController.cs
+++ Backend/Controllers/PromoCodeController.cs
@@ -1,6 +1,10 @@
-﻿using Backend.Services;
+﻿using Backend.Contexts;
+using Backend.Models;
+using Backend.Models.Entities;
+using Backend.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
@@ -8,24 +12,39 @@ namespace Backend.Controllers
     [ApiController]
     public class PromoCodeController : ControllerBase
     {
-        private readonly PromoCodeService _promoCodeService;
+        //private readonly PromoCodeService _promoCodeService;
+        private readonly NoSqlContext _nosql;
 
-        public PromoCodeController(PromoCodeService promoCodeService)
+        public PromoCodeController( NoSqlContext nosql)
         {
-            _promoCodeService = promoCodeService;
+            //_promoCodeService = promoCodeService;
+            _nosql = nosql;
         }
 
-        // [HttpGet]
-        // GettAll
-
-        [HttpPost("generate")]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var promoCodes = new List<PromoCodeEntity>();
+            foreach (var promoCode in await _nosql.PromoCode.ToListAsync())
+                promoCodes.Add(new PromoCodeEntity
+                {
+                    Id = promoCode.Id,
+                    Name = promoCode.Name,
+                    Discount = promoCode.Discount,
+                    ExpiryDate = promoCode.ExpiryDate
+                });
+            //return new OkObjectResult(promoCodes);
+            return Ok(promoCodes);
+        }
+        /*
+            [HttpPost("generate")]
         public IActionResult GeneratePromoCode()
         {
             var promoCode = _promoCodeService.GeneratePromoCode();
 
             return Ok (promoCode);
         }
-
+        */
 
         // [HttpDelete]
     }
