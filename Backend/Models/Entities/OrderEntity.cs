12d84b34e8706f703666f5d51482de8ca5076a41
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-19 12:45:18 +0200

    updated OrderEntity

--- Backend/Models/Entities/OrderEntity.cs
+++ Backend/Models/Entities/OrderEntity.cs
@@ -11,6 +11,6 @@
         public string PaymentMethod { get; set; } = null!;
         public string? Comment { get; set; }
         public string Delivery { get; set; } = null!;
-        public List<PromoCodeEntity> PromoCodes { get; set; } = new List<PromoCodeEntity>();
+        public List<PromoCodeEntity>? PromoCodes { get; set; } = new List<PromoCodeEntity>();
     }
 }
