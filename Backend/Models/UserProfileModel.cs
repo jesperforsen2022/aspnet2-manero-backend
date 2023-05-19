e271384e978264f38ec5d558ac2ad7414f6b0dfc
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-19 12:35:08 +0200

    added city, streetname and postalcode on UserProfileModel

--- Backend/Models/UserProfileModel.cs
+++ Backend/Models/UserProfileModel.cs
@@ -6,6 +6,11 @@ namespace Backend.Models
     {
         public string Email { get; set; } = null!;
         public string Name { get; set; } = null!;
+        public string StreetName { get; set; } = null!;
+
+        public string PostalCode { get; set; } = null!;
+
+        public string City { get; set; } = null!;
         public string? PhoneNumber { get; set; }
         public string? ImageSrc { get; set; }
 
