a85ca7f3047190929d9b10fcf6ee57d4a9339fd6
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-23 11:04:16 +0200

    Updated OrderController

--- Backend/Controllers/OrderController.cs
+++ Backend/Controllers/OrderController.cs
@@ -12,7 +12,6 @@ namespace Backend.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
-        //private readonly NoSqlContext _nosql;
         private readonly OrderService _orderService;
 
         public OrderController(OrderService orderService)
@@ -23,74 +22,23 @@ namespace Backend.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            //var orders = new List<OrderModel>();
-            //foreach (var order in await _nosql.Orders.ToListAsync())
-            //    orders.Add(new OrderModel
-            //    {
-            //        Id = order.Id,
-            //        Price = order.Price,
-            //        Date = order.Date,
-            //        Profile = order.Profile,
-            //        Products = order.Products,
-            //        OrderStatus = order.OrderStatus,
-            //        PaymentMethod = order.PaymentMethod,
-            //        Comment = order.Comment,
-            //        Delivery = order.Delivery,
-            //        PromoCodes = order.PromoCodes
-            //    });
-            //return Ok(orders);
-            return Ok(_orderService.GetAllOrders());
+            var orders = await _orderService.GetAllOrders();
+            return Ok(orders);
         }
 
         [HttpGet("{email}")]
         public async Task<IActionResult> Get(string email)
         {
-            //var ordersOfUser = new List<OrderModel>();
-
-            //foreach (var order in await _nosql.Orders.ToListAsync()) 
-            //{ 
-            //    if (order.Profile.Email == email) 
-            //    {
-            //        ordersOfUser.Add(new OrderModel
-            //        {
-            //            Id = order.Id,
-            //            Price = order.Price,
-            //            Date = order.Date,
-            //            Profile = order.Profile,
-            //            Products = order.Products,
-            //            OrderStatus = order.OrderStatus,
-            //            PaymentMethod = order.PaymentMethod,
-            //            Comment = order.Comment,
-            //            Delivery = order.Delivery,
-            //            PromoCodes = order.PromoCodes
-            //        });
-            //    }
-            //}
-            //return Ok(ordersOfUser);
-            return Ok(_orderService.GetOrder(email));
+            var ordersOfUser = await _orderService.GetOrder(email);
+            return Ok(ordersOfUser);
         }
 
 
         [HttpPost]
         public async Task<IActionResult> Create(OrderModel order)
         {
-            //OrderEntity orderEntity = new ()
-            //{
-            //    Id = Guid.NewGuid(),
-            //    Price = order.Price,
-            //    Date = order.Date,
-            //    Profile = order.Profile,
-            //    Products = order.Products,
-            //    OrderStatus = order.OrderStatus,
-            //    PaymentMethod = order.PaymentMethod,
-            //    Comment = order.Comment,
-            //    Delivery = order.Delivery,
-            //    PromoCodes = order.PromoCodes
-            //};
-            //_nosql.Orders.Add(orderEntity);
-            //await _nosql.SaveChangesAsync();
-            //return Ok();
-            return Ok(_orderService.CreateOrder(order));
+            var orderEntity = await _orderService.CreateOrder(order);
+            return Ok(orderEntity);
         }
 
     }
