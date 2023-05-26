3284ea49b571a7a9fed1a79f57cb409f20de0f0c
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-26 16:11:39 +0200

    removed Image

--- Backend/Models/OrderProductModel.cs
+++ Backend/Models/OrderProductModel.cs
@@ -3,7 +3,6 @@
     public class OrderProductModel
     {
         public Guid Id { get; set; }
-        public string? Image { get; set; }
         public string? Name { get; set; }
 
         public decimal? Price { get; set; }
