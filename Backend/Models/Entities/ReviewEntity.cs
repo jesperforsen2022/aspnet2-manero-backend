05c1b99700438955b57f0de29f462fcadf483520
OzAlbayrak <89449180+OzAlbayrak@users.noreply.github.com>
2023-05-27 13:58:13 +0200

    added ReviewController, updated ReviewModel and ReviewEntity

--- Backend/Models/Entities/ReviewEntity.cs
+++ Backend/Models/Entities/ReviewEntity.cs
@@ -1,10 +1,14 @@
-﻿namespace Backend.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.Entities
 {
     public class ReviewEntity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public decimal? RatingStar { get; set; } = 0;
-
-        public List<ReviewModel>? Reviews { get; set; } = new List<ReviewModel>();
+        public string Reviewer { get; set; } = null!;
+        public string? Review { get; set; }
+        [Required]
+        public decimal RatingStar { get; set; }
+        public DateTime ReviewDate { get; set; } = DateTime.UtcNow;
     }
 }
--- Backend/Models/ReviewModel.cs
+++ Backend/Models/ReviewModel.cs
@@ -1,10 +1,12 @@
-﻿namespace Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
 {
     public class ReviewModel
     {
-        public string? ReviewerEmail { get; set; }
+        public string? Reviewer { get; set; }
         public string? Review { get; set; }
-        public decimal? RatingStar { get; set; } = 0;
-
+        public decimal? RatingStar { get; set; }
+        public DateTime? ReviewDate { get; set; }
     }
 }
