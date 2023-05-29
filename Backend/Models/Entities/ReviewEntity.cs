691822be90d09a8b977cf5aad8b8be6c3038e640
Sam Jaudat <112482389+samjaudat@users.noreply.github.com>
2023-05-29 13:33:27 +0200

    updated

--- Backend/Models/Entities/ReviewEntity.cs
+++ Backend/Models/Entities/ReviewEntity.cs
@@ -1,10 +1,13 @@
-﻿namespace Backend.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.Entities
 {
     public class ReviewEntity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Reviewer { get; set; } = null!;
         public string? Review { get; set; }
+
         [Required]
         public decimal RatingStar { get; set; }
         public DateTime ReviewDate { get; set; } = DateTime.UtcNow;
