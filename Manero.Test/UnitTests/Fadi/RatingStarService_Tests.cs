e7dfff20b7630beccf577afe05254dbfce544d26
Fadi-Jawhar <fadi.jawhr@hotmail.com>
2023-06-01 13:49:24 +0200

    Fadis Test

---
+++ Manero.Test/UnitTests/Fadi/RatingStarService_Tests.cs
@@ -0,0 +1,44 @@
+﻿using Backend.Services;
+using Manero.Test.UnitTests.Fadi;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manero.Test.UnitTests.Fadi
+{
+    public class RatingStarService_Tests
+    {
+        private Mock<RatingStarService> _ratingStarServiceMock;
+
+        public RatingStarService_Tests()
+        {
+            _ratingStarServiceMock = new Mock<RatingStarService>();
+        }
+
+        [Fact]
+        public void AverageRatingStar_Should_Return_Correct_Average()
+        {
+            // Arrange
+            var ratingStarService = new RatingStarService();
+            var ratingStarList = new List<float> { 2.5f, 3.0f, 4.5f, 5.0f };
+            var expectedAverage = 3.75f;
+
+            // Act
+            var actualAverage = ratingStarService.AverageRatingStar(ratingStarList);
+
+            // Assert
+            Assert.Equal(expectedAverage, actualAverage);
+        }
+      
+    }
+}
+//private RatingStarService _ratingStarService;
+//private Mock<RatingStarService> _ratingStarServiceMock;
+
+//public RatingStarService_Tests()
+//{
+//    _ratingStarServiceMock = new Mock<RatingStarService>();
+//}
